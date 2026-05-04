6596f63ac32601f250ff8f1aa8e4f2f6801687db
ankitsingulr <ankitk@singulr.ai>
2026-05-04 12:11:13 +0530

    Add new fields to tenant details in JSON

--- updating_json.py
+++ updating_json.py
@@ -1,6 +1,5 @@
 import json
 import os
-import boto3
 import sys
 
 platform = sys.argv[1]
@@ -8,19 +7,26 @@ collectors = json.loads(sys.argv[2])
 tenant_id = sys.argv[3]
 tenant_endpoint = sys.argv[4]
 customer_name = sys.argv[5]
+distribution_id = sys.argv[6]
+bucket_name = sys.argv[7]
+descope_project_id = sys.argv[8]
 
-
-with open('../tenant_details.json', 'r') as file:
+with open("test/tenant_details.json", "r") as file:
     data = json.load(file)
 
 new_tenant = {
     "platform": platform,
     "collectors": collectors,
     "tenant_id": tenant_id,
-    "tenant_endpoint": tenant_endpoint
-} 
+    "tenant_endpoint": tenant_endpoint,
+    "ui": {
+        "bucket": bucket_name,
+        "distribution": distribution_id,
+        "descope_project_id": descope_project_id,
+    },
+}
 
 data[customer_name] = new_tenant
 
-with open('../tenant_details.json', 'w') as file:
+with open("test/tenant_details.json", "w") as file:
     json.dump(data, file, indent=4)
